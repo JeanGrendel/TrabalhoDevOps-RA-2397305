fe91823d2f755f32e7c2a74216e71d2da2cd5e0c
JeanGrendel <jeanbgrendel@gmail.com>
2024-12-01 23:13:02 -0300

    Ajustes gerais nos arquivos

--- tests/test_aluno.py
+++ tests/test_aluno.py
@@ -11,5 +11,17 @@ def client():
         yield client
 
 def test_adicionar_aluno(client):
-    response = client.post('/alunos', json={"nome": "Alex", "ra": "271823"})
+    response = client.post('/alunos', json={
+        "nome": "Alex",
+        "sobrenome": "Oliveira",
+        "turma": "4F",
+        "disciplinas": "Devops, Java"
+    })
     assert response.status_code == 201
+    assert b"Aluno adicionado com sucesso!" in response.data
+
+def test_listar_alunos(client):
+    response = client.get('/alunos')
+    assert response.status_code == 200
+    alunos = response.get_json()
+    assert len(alunos) > 0
